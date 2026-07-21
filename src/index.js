4ecda22a861bfadf5738749dd5aa154c51102b5e
PwannMalin <89216512+PwannMalin@users.noreply.github.com>
2026-07-20 19:53:50 -0500

    Add browser chat UI

--- src/index.js
+++ src/index.js
@@ -205,7 +205,7 @@ app.get("/test/search/models", async (req, res) => {
 
 app.get("/test/rental-lookups", async (req, res) => {
 
-    const tool = toolSource["search_models"];
+    const tool = toolSource["search.execute"];
 
     const result = await tool.handler({
         searchText: ""
