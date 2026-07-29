0d48e5398cbdff6850e0ac790e7572b9905fb39b
PwannMalin <89216512+PwannMalin@users.noreply.github.com>
2026-07-29 16:24:40 -0500

    Update chat UI and logo

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,12 +2,16 @@ import {
     PublicClientApplication
 } from "@azure/msal-browser";
 
-import {
-    msalConfig,
-    loginRequest
-} from "./msalConfig.js";
+const msalConfig = {
+    auth: {
+        clientId: "193e3b30-e9cd-43a8-9a03-a32b1b22cf5f",
+        authority:
+            "https://login.microsoftonline.com/20bd77a5-e148-4b27-bed3-93baf486481b",
+        redirectUri: window.location.origin
+    }
+};
 
-const msalInstance =
+export const msalInstance =
     new PublicClientApplication(msalConfig);
 
 await msalInstance.initialize();
